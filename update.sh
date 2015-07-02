f843fa88a933b9c4e08c1093f935ae793766e84e
everbot <everbot@live.com>
2015-07-02 09:41:00 +0930

    Add update script to pull from upstream

---
+++ update.sh
@@ -0,0 +1,27 @@
+git status
+git subtree pull --prefix bundle/vim-fugitive https://github.com/tpope/vim-fugitive.git master --squash
+
+git status
+git subtree pull --prefix bundle/unite.vim https://github.com/Shougo/unite.vim.git master --squash
+
+git status
+git subtree pull --prefix bundle/vimproc.vim https://github.com/Shougo/vimproc.vim.git master --squash
+
+git status
+git subtree pull --prefix bundle/neocomplete.vim https://github.com/Shougo/neocomplete.vim.git master --squash
+
+git status
+git subtree pull --prefix bundle/neosnippet.vim https://github.com/Shougo/neosnippet.vim.git master --squash
+
+git status
+git subtree pull --prefix bundle/unite-outline https://github.com/Shougo/unite-outline.git master --squash
+
+git status
+git subtree pull --prefix bundle/vim-airline https://github.com/bling/vim-airline.git master --squash
+
+git status
+git subtree pull --prefix bundle/vim-snippets https://github.com/honza/vim-snippets.git master --squash
+
+git status
+git subtree pull --prefix bundle/unite-tag https://github.com/tsukkee/unite-tag.git master --squash
+
